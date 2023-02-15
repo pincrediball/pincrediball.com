8ac96b3382fdbcf1ad60f130c82cda3d4908e1bf
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-02-15 17:20:31 +0100

    Create initial build

---
+++ docs/_app/immutable/chunks/4-5058b79c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/devlog/_page.svelte-c8b6ca2a.js";export{t as component};
---
+++ docs/_app/immutable/chunks/stores-4f6e7f00.js
@@ -0,0 +1,1 @@
+import"./index-2c8c2d89.js";import{s as e}from"./singletons-3f62c808.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
