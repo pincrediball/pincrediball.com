9210b61869faa3a0fe036e657bed37ccdec6768e
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-28 09:17:52 +0200

    Tiny devlog improvements

--- docs/_app/immutable/chunks/1.71771d6a.mjs
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_error.svelte.c60fd73f.mjs";export{t as component};
---
+++ docs/_app/immutable/chunks/1.7d64c8a0.mjs
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_error.svelte.35b5c19d.mjs";export{t as component};
