7c6ad83fac6adddbe0c04b3694229efca2d4d95b
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-30 22:07:04 +0200

    Improve responsiveness
    The tables pushed small devices and viewports to have a horizontal
    scroll bar. Let's hide some info instead, that's worth the trade-off.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  safelist: ['sm:block sm:hidden block hidden'],
   theme: {
     extend: {
       boxShadow: {
